e69b1a02dd24e660b51a08fc3f94fccb25bfa6e3
Chris M <chris@adadevelopersacademy.org>
2020-11-27 17:34:50 -0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,9 @@ const presets = [
         firefox: "60",
         chrome: "67",
         safari: "11.1",
+        useBuiltIns: "usage",
+        corejs: "3.8.0",
       },
-      useBuiltIns: "usage",
     },
   ],
 ];
